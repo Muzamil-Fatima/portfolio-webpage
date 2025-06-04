5a242a28908a61c072d1fa0d60450819199433d1
Muzamil <muzamilzubair987654@gmail.com>
2025-06-05 00:29:23 +0500

    Simple Portfolio web-page

---
+++ script.js
@@ -0,0 +1,6 @@
+// Basic JavaScript to handle form submission (you can expand this for more functionality)
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Your message has been sent!");
+  });
+  
